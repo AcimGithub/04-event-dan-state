d59c24b40477ad4583ff251cfa697d5246b2af7e
Lukman Hakim <madlukmanhakim@gmail.com>
2024-03-13 14:08:44 +0700

    Praktikum 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 "use client";
-import Tombol_1, {Tombol_2} from "@/components/button";
+import Tombol_1, {Tombol_2, Tombol_3} from "@/components/button";
 
 
 export default function Home() {
@@ -12,6 +12,11 @@ export default function Home() {
         <hr>
         </hr>
         <Tombol_2 isiPesan="Ini pesan dari Presiden :)" namaTombol="Coba Pesan"/>
+     </div>
+     <br />
+     <div className="bg-red300" onClick={() => alert('Parent Element : Div')}>
+        <Tombol_3 isiPesan="Child Element : Tombol-1" namaTombol="Tombol-1"/>
+        <Tombol_3 isiPesan="Child Element : Tombol-2" namaTombol="Tombol-2"/>
      </div>
     </>
   );
